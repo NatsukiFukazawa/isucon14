658f609d25cdc3c1376a1fd1539718a09a2a35a8
sugiwa <sugi.warra@gmail.com>
2024-12-08 17:31:06 +0900

    fix

--- app/webapp/go/ride_status.go
+++ app/webapp/go/ride_status.go
@@ -99,7 +99,9 @@ func GetCacheLatestRideStatus(rideId string) (rideStatus *RideStatus, found bool
 	defer mu.RUnlock()
 
 	rideStatusInfo, found := rideStatusCacheMap[rideId]
-	rideStatus = rideStatusInfo.RideStatus
+	if found {
+		rideStatus = rideStatusInfo.RideStatus
+	}
 	return
 }
 
