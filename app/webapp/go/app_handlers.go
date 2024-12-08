50fe50265eee1a814d9e70c7f1020e5a68b6441a
iyoshiha <yoshi3co@gmail.com>
2024-12-08 16:55:38 +0900

    retry

--- app/webapp/go/app_handlers.go
+++ app/webapp/go/app_handlers.go
@@ -683,7 +683,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-				RetryAfterMs: 2500,
+				RetryAfterMs: 2300,
 			})
 			return
 		}
